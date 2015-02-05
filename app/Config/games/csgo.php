18cd185ca43a95ea7993478b5318542661f63e38
Thomaxius <Tommi.Santamaa@gmail.com>
2015-02-05 13:22:53 +0200

    Update csgo.php

--- app/Config/games/csgo.php
+++ app/Config/games/csgo.php
@@ -11,7 +11,7 @@
  * @package       app.Config.games
  * @since         XLRstats v3.0
  * @version       0.1
- * Credits:       Half assed port from cod2.php by Thomaxius, all of the art are taken from 
+ * Credits:       Half assed port from cod2.php by Thomaxius, all of the art is taken from 
  * 		  http://counterstrike.wikia.com/
  */
 
